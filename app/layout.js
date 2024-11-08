0c848887368cdf3b7af6727de8caed2a17df3b64
siuupriyanshu <mandalpriyanshu22@gmail.com>
2024-11-08 14:17:37 +0545

    home

--- app/layout.js
+++ app/layout.js
@@ -22,9 +22,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-purple-50`}
       >
-       
+          <Navbar />
         {children}
       </body>
     </html>
