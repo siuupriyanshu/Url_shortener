5887217f720c3709aa5a935bfa4e4a2edf56c90a
siuupriyanshu <mandalpriyanshu22@gmail.com>
2024-11-09 08:49:57 +0545

    /generate

---
+++ app/shorten/page.js
@@ -0,0 +1,34 @@
+"use client"
+import React, { useState } from 'react'
+
+const Shorten = () => {
+    const [url, seturl] = useState("")
+    const [shorturl, setshorturl] = useState("")
+    const [generated, setGenerated] = useState("")
+
+    const generate = () => {
+
+    }
+
+    return (
+        <div className='mx-auto max-w-lg bg-purple-100 my-16 p-8 rounded-lg flex flex-col gap-4'>
+            <h1 className='font-bold text-2xl'>Generate your short URLs</h1>
+            <div className='flex flex-col gap-2'>
+                <input type="text"
+                    value={url}
+                    className='px-4 py-2 focus:outline-purple-600 rounded-md'
+                    placeholder='Enter your URL'
+                    onChange={e => { seturl(e.target.value) }} />
+
+                <input type="text"
+                    value={shorturl}
+                    className='px-4 py-2 focus:outline-purple-600 rounded-md'
+                    placeholder='Enter your preferred short URL text'
+                    onChange={e => { setshorturl(e.target.value) }} />
+                <button onClick={generate} className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 my-3 font-bold text-white'>Generate</button>
+            </div>
+        </div>
+    )
+}
+
+export default Shorten
